35380a3075691ae7ffda50d436cac9fc5d6f0607
neutron18 <neutron.ycn@gmail.com>
2020-01-07 13:40:21 -0500

    Completed search service 1.0

--- src/main/java/cmu/youchun/recommender/dao/ShopModelMapper.java
+++ src/main/java/cmu/youchun/recommender/dao/ShopModelMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public interface ShopModelMapper {
     /**
@@ -60,5 +61,14 @@ public interface ShopModelMapper {
     Integer countAll();
 
     List<ShopModel> recommend(@Param("longitude") BigDecimal longitude, @Param("latitude") BigDecimal latitude);
+    List<ShopModel> search(@Param("longitude") BigDecimal longitude,
+                           @Param("latitude") BigDecimal latitude,
+                           @Param("keyword")String keyword,
+                           @Param("orderby")Integer orderby,
+                           @Param("categoryId")Integer categoryId,
+                           @Param("tags")String tags);
 
+    List<Map<String,Object>> searchGroupByTags(@Param("keyword")String keyword,
+                                               @Param("categoryId")Integer categoryId,
+                                               @Param("tags")String tags);
 }
--- src/main/java/cmu/youchun/recommender/service/ShopService.java
+++ src/main/java/cmu/youchun/recommender/service/ShopService.java
@@ -5,6 +5,7 @@ import cmu.youchun.recommender.model.ShopModel;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public interface ShopService {
 
@@ -31,6 +32,28 @@ public interface ShopService {
     /**
      * Recommend
      */
-
     List<ShopModel> recommend(BigDecimal longitude, BigDecimal latitude);
+
+
+    /**
+     * Search using a keyword, enabling to select orders and filters.
+     * @param longitude
+     * @param latitude
+     * @param keyword
+     * @param orderby
+     * @param categoryId
+     * @param tags
+     * @return
+     */
+    List<ShopModel> search(BigDecimal longitude,BigDecimal latitude,
+                           String keyword,Integer orderby,Integer categoryId,String tags);
+
+    /**
+     * Search a keyword, group the results by tags.
+     * @param keyword
+     * @param categoryId
+     * @param tags
+     * @return
+     */
+    List<Map<String,Object>> searchGroupByTags(String keyword, Integer categoryId, String tags);
 }
